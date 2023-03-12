88db7bd40297254ec2ecc415731604f7ddeabfba
oierIM <98800526+oierIM@users.noreply.github.com>
2023-03-12 18:13:21 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -4,9 +4,9 @@ import os
 import socket
 import json
 
-# Connect to mariadb
-mariadb_konexioa = mariadb.connect(host="db", user='user', password='pass', database='Proba') #Konexioa sortu, beharrezko host,erabiltzaile,pasahitz eta datubaseren izena pasatuz parametro bezala
-cursor = mariadb_konexioa.cursor() #Kontsultak egiteko beharrezkoa izango den kurtsorea sortu
+#Konexioa sortu, beharrezko host,erabiltzaile,pasahitz eta datubaseren izena pasatuz parametro bezala
+mariadb_konexioa = mariadb.connect(host="db", user='user', password='pass', database='Proba') 
+cursor = mariadb_konexioa.cursor()#Kontsultak egiteko beharrezkoa izango den kurtsorea sortu
 app = Flask(__name__) #Flask instantzia sortu
 
 @app.route("/db") #Adminer kontainerrera joateko eskaeraren ruta
@@ -18,15 +18,18 @@ def message():
 	if request.method == 'GET': #Datu basearen erregistro guztiak bueltatu
 		cursor.execute("SELECT * FROM Informazioa") #Beharrezko taularen gainean eskaera exekutatu
 		return cursor.fetchall() #Lortutako emaitzak bueltatzeko beharrezko komandoa
-	elif request.method == 'POST': #Eskaeraren edukia datu basean gordeko du. Json formatuan egongo da edukia
-		eskaera = request.json #eskaeraren edukia gordetzen dugu
-		Id = socket.gethostname() #Uneko flask kontainerraren id-a lortu
-		cursor.execute(f"INSERT INTO Informazioa (From_eskaera, Content_eskaera, Id) VALUES (%s, %s, %s)",(eskaera['From'],eskaera['Content'], Id)) #Datu basean eskaerako From, Content eta kontainerraren id-a gordeko dugu tupla batean
-		cursor.execute("commit") #Aldaketak burutu
-		return 0
+	elif request.method == 'POST': #Eskaeraren edukia datu basean gordeko du. Json formatuko edukia
+		from_eskaera = request.json["From"] #eskaeraren edukia gordetzen dugu
+		content_eskaera = request.json["Content"] #eskaeraren edukia gordetzen dugu
+		id = socket.gethostname() #Uneko flask kontainerraren id-a lortu
+		query = ("INSERT INTO Informazioa (From_eskaera, Content_eskaera, Id)"
+               		"VALUES (%s, %s, %s)") #Gordetzeko query-a
+		cursor.execute(query, (from_eskaera, content_eskaera, id)) #Exekutatu query-a
+		mariadb_konexioa.commit() #Aldaketak burutu
+		return "Gordeta"
 
-@app.route("/test", methods=['GET']) #Test rutako eskaerak ALIVE testua bueltatuko digu, bere flask kontainerraren id-arekin batera
-def test():
+@app.route("/test", methods=['GET']) #Test rutako eskaera
+def test(): #ALIVE testua bueltatuko digu, bere flask kontainerraren id-arekin batera
 	return "ALIVE "+ socket.gethostname() #bueltatu alive eta kontainerraren id-a
 
 
