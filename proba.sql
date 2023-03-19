72eedfdc2ed4eb2cc51eaf439b1236d32f8d9dc3
oierIM <98800526+oierIM@users.noreply.github.com>
2023-03-19 11:51:42 +0100

    Create proba.sql

---
+++ proba.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE IF NOT EXISTS Proba;
+USE Proba;
+CREATE TABLE IF NOT EXISTS Informazioa (
+        From_eskaera varchar(255),
+        Content_eskaera varchar(255),
+        Id varchar(255));
